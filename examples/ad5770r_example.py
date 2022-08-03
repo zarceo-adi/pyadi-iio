da0608bd5e1b67e329760fd15491efd7cf3a191e
Zuedmar Arceo <75724878+zarceo-adi@users.noreply.github.com>
2022-08-03 19:17:12 +0800

    Added ad5770r_example.py support

---
+++ examples/ad5770r_example.py
@@ -0,0 +1,33 @@
+
+import adi
+
+# Set up AD5770r
+#myad5770r = adi.ad5770r(uri="ip:analog.local")
+myad5770r = adi.ad5770r(uri="ip:10.116.171.104")
+
+for ch in myad5770r.channel:
+    print("Channel Name: ", ch.name)
+
+    raw_val = ch.raw
+    print("\traw: {}".format(raw_val))
+
+    scale_val = ch.scale
+    print("\tscale: {}".format(scale_val))
+
+    low_pass_filter_val = ch.filter_low_pass_3db_frequency
+    print("\tfilter_low_pass_3db_frequency: {}".format(low_pass_filter_val))
+
+    low_pass_filter_avail_val = ch.filter_low_pass_3db_frequency_available
+    print("\tfilter_low_pass_3db_frequency_available: {}".format(low_pass_filter_avail_val))
+
+    offset_val = ch.offset
+    print("\toffset: {}".format(offset_val))
+
+    powerdown_val = ch.powerdown
+    print("\tpowerdown: {}".format(powerdown_val))
+
+
+
+
+
+
